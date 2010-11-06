f0ce2d574b1107787f1fd254563a8a8fcd5c1a99
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2010-11-06 17:02:23 +0100

    added username / password inputs

--- src/main/java/se/jimlar/ValtechIntraSync.java
+++ src/main/java/se/jimlar/ValtechIntraSync.java
@@ -1,23 +1,62 @@
 package se.jimlar;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.List;
 
 public class ValtechIntraSync extends Activity {
+    private SharedPreferences preferences;
+    private static final String USERNAME_SETTING = "username";
+    private static final String PASSWORD_SETTING = "password";
+
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-        TextView tv = new TextView(this);
 
-        List<Employee> employeeSet = new APIClient("jimmy.larsson", "", new APIResponseParser()).getEmployees();
+        preferences = getSharedPreferences("valtechdroid",  MODE_PRIVATE);
+        String savedUsername = preferences.getString(USERNAME_SETTING, "");
+        String savedPassword = preferences.getString(PASSWORD_SETTING, "");
 
-        tv.setText("Hello, Android, from IntelliJ IDEA!\n" + employeeSet.size() + " employees found");
-        setContentView(tv);
+        setContentView(R.layout.main);
 
+        getUsername().setText(savedUsername);
+        getPassword().setText(savedPassword);
+
+        Button button = (Button) findViewById(R.id.ok);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                List<Employee> employeeSet = new APIClient(getUsername().getText().toString(),
+                                                           getPassword().getText().toString(),
+                                                           new APIResponseParser()).getEmployees();
+
+                TextView status = (TextView) findViewById(R.id.status);
+                status.setText(employeeSet.size() + " employes found");
+            }
+        });
     }
 
+    private EditText getPassword() {
+        return (EditText) findViewById(R.id.passwordentry);
+    }
 
+    private EditText getUsername() {
+        return (EditText) findViewById(R.id.userentry);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences.Editor ed = preferences.edit();
+        ed.putString(USERNAME_SETTING, getUsername().getText().toString());
+        ed.putString(PASSWORD_SETTING, getPassword().getText().toString());
+        ed.commit();
+    }
 }

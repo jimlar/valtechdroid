54492838061e4d24fb346c922eb82c28108a9b3c
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2011-02-09 14:04:48 +0100

    now not downloading image over and over

---
+++ src/main/java/se/jimlar/Debugger.java
@@ -0,0 +1,33 @@
+package se.jimlar;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+
+public class Debugger {
+    private static final Logger LOG = new Logger(Debugger.class);
+
+    public static void dumpContactTables(ContentResolver contentResolver) {
+        dumpTable(contentResolver, ContactsContract.Data.CONTENT_URI, null);
+        dumpTable(contentResolver, ContactsContract.Groups.CONTENT_URI, null);
+        dumpTable(contentResolver, ContactsContract.RawContacts.CONTENT_URI, null);
+    }
+
+    public static void dumpTable(ContentResolver contentResolver, Uri contentUri, String selection) {
+        LOG.debug(contentUri.toString());
+        LOG.debug("-------------------------");
+        Cursor cursor = contentResolver.query(contentUri, null, selection, null, null);
+
+        int columns = cursor.getColumnCount();
+        while (cursor.moveToNext()) {
+            String data = "";
+            for (int i = 0; i < columns; i++) {
+                data += cursor.getColumnName(i) + ": " + cursor.getString(i) + ", ";
+            }
+            LOG.debug(data);
+        }
+        cursor.close();
+        LOG.debug("-------------------------");
+    }
+}

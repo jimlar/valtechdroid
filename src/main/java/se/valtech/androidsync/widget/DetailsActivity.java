a0dd8fa639119c94794b0dbcb704569805274fc5
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2011-06-13 14:47:21 +0200

    added a details view opened by clicking the widget or in the app meny

--- src/main/java/se/valtech/androidsync/widget/DetailsActivity.java
+++ src/main/java/se/valtech/androidsync/widget/DetailsActivity.java
@@ -1,36 +1,21 @@
 package se.valtech.androidsync.widget;
 
 import android.accounts.AccountAuthenticatorActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
 import se.valtech.androidsync.Logger;
 import se.valtech.androidsync.R;
 
-public class DetailsActivity extends AccountAuthenticatorActivity {
+public class DetailsActivity extends Activity {
     private static final Logger LOGGER = new Logger(DetailsActivity.class);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         LOGGER.info("create");
-        setContentView(R.layout.main);
+        setContentView(R.layout.status_appwidget_details);
 
-        username = (EditText) findViewById(R.id.username);
-        password = (EditText) findViewById(R.id.password);
-
-        loginButton = (Button) findViewById(R.id.login);
-        loginButton.setOnClickListener(new OnClickListener() {
-
-            public void onClick(View v) {
-                LOGGER.info("click");
-                String user = username.getText().toString();
-                String password = DetailsActivity.this.password.getText().toString();
-
-            }
-
-        });
+        View view = findViewById(R.id.widgetdetails);
     }
 }
